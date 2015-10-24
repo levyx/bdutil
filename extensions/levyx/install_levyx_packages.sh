20a18dd8ad3148f734c3e7cae0efece52989da3b
semad <semad@users.noreply.github.com>
2015-10-23 17:25:54 -0700

    Update install_levyx_packages.sh

--- extensions/levyx/install_levyx_packages.sh
+++ extensions/levyx/install_levyx_packages.sh
@@ -7,7 +7,7 @@
 
 yum -y install epel-release
 yum -y install tree vim wget sysstat mdadm lsof screen wget psmisc net-tools nmap-ncat collectd git
-yum -y install gcc zlib-devel zip unzip
+yum -y install gcc zlib-devel zip unzip flex bycc
 #yum -y install maven emacs nginx
 
 setenforce permissive
