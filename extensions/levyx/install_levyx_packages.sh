9e59ab9b1cfda1209151a464b6f9f0155e52bfce
Shahram Emadi <shahram@levyx.com>
2016-11-14 09:54:59 -0800

    Add the local disk to /mnt/ed

--- extensions/levyx/install_levyx_packages.sh
+++ extensions/levyx/install_levyx_packages.sh
@@ -78,3 +78,67 @@ chmod +w /etc/sudoers
 sed -i 's/Defaults\s\+requiretty/#Defaults requiretty/' /etc/sudoers
 sed -i 's/Defaults\s\+!visiblepw/#Defaults !visiblepw/' /etc/sudoers
 chmod -w /etc/sudoers
+
+set -e
+set -x 
+
+# Get a list of disks from the metadata server.
+BASE_DISK_URL='http://metadata.google.internal/computeMetadata/v1/instance/disks/'
+DISK_PATHS=$(curl_v1_metadata "${BASE_DISK_URL}")
+MOUNTED_DISKS=()
+
+for DISK_PATH in ${DISK_PATHS}; do
+  # Use the metadata server to determine the official index/name of each disk.
+  DISK_NAME=$(curl_v1_metadata "${BASE_DISK_URL}${DISK_PATH}device-name")
+  DISK_INDEX=$(curl_v1_metadata "${BASE_DISK_URL}${DISK_PATH}index")
+  DISK_TYPE=$(curl_v1_metadata "${BASE_DISK_URL}${DISK_PATH}type")
+
+  # Index '0' is the boot disk and is thus already mounted.
+  if [[ "${DISK_INDEX}" == '0' ]]; then
+    echo "Boot disk is ${DISK_NAME}; will not attempt to mount it."
+    continue
+  fi
+
+  
+  if [[ "${DISK_TYPE}" == 'LOCAL-SSD' ]]; then
+    DISK_PREFIX='ed'
+  elif [[ "${DISK_TYPE}" == 'PERSISTENT-SSD' ]]; then
+    DISK_PREFIX='pd'
+    continue
+  fi
+
+  # The metadata-specified 'name' can be converted to a disk 'id' by prepending
+  # 'google-' and finding it under /dev/disk/by-id.
+  DISK_ID="/dev/disk/by-id/google-${DISK_NAME}"
+  echo "Resolved disk name '${DISK_NAME}' to expected path '${DISK_ID}'."
+
+  # We will name the mount-point after the official 'disk index'; this means
+  # there will be no mounted disk with suffix '0' since '0' is the boot disk.
+  DATAMOUNT="/mnt/${DISK_PREFIX}${DISK_INDEX}"
+  mkdir -p ${DATAMOUNT}
+  MOUNTED_DISKS+=(${DATAMOUNT})
+  echo "Mounting '${DISK_ID}' under mount point '${DATAMOUNT}'..."
+  #$MOUNT_TOOL=/usr/share/google/safe_format_and_mount
+  #${MOUNT_TOOL} -m 'mkfs.ext4 -F' ${DISK_ID} ${DATAMOUNT}
+  mkfs.ext4 -F ${DISK_ID} 
+  mount ${DISK_ID} ${DATAMOUNT}
+
+  # Idempotently update /etc/fstab
+  if cut -d '#' -f 1 /etc/fstab | grep -qvw ${DATAMOUNT}; then
+    DISK_UUID=$(blkid ${DISK_ID} -s UUID -o value)
+    MOUNT_ENTRY=($(grep -w ${DATAMOUNT} /proc/mounts))
+    # Taken from /usr/share/google/safe_format_and_mount
+    MOUNT_OPTIONS='defaults,discard'
+    echo "UUID=${DISK_UUID} ${MOUNT_ENTRY[@]:1:2} ${MOUNT_OPTIONS} 0 2 \
+        # added by bdutil" >> /etc/fstab
+  fi
+done
+
+# If disks are mounted use the first one to hold target of symlink /hadoop
+#if (( ${#MOUNTED_DISKS[@]} )); then
+#  MOUNTED_HADOOP_DIR=${MOUNTED_DISKS[0]}/hadoop
+#  mkdir -p ${MOUNTED_HADOOP_DIR}
+#  if [[ ! -d /hadoop ]]; then
+#    ln -s ${MOUNTED_HADOOP_DIR} /hadoop
+#  fi
+#fi
